75457abbeb53b32cf80dbef62597076742a137c9
xuniorss <fortunatojunior2020@gmail.com>
2023-05-17 23:17:17 -0300

    terceiro dia

--- app/index.tsx
+++ app/index.tsx
@@ -4,23 +4,63 @@ import {
    Roboto_700Bold,
    useFonts,
 } from '@expo-google-fonts/roboto'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
-
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-
 import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import blurBg from '../src/assets/bg-blur.png'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import Stripes from '../src/assets/stripes.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+   tokenEndpoint: 'https://github.com/login/oauth/access_token',
+   revocationEndpoint:
+      'https://github.com/settings/connections/applications/a51f3bea66f8974e11c3',
+}
+
 export default function App() {
+   const router = useRouter()
+
    const [hasLoadedFonts] = useFonts({
       Roboto_400Regular,
       Roboto_700Bold,
       BaiJamjuree_700Bold,
    })
+
+   const [, response, promptAsync] = useAuthRequest(
+      {
+         clientId: 'a51f3bea66f8974e11c3',
+         scopes: [''],
+         redirectUri: makeRedirectUri({ scheme: 'nlwspacetime' }),
+      },
+      discovery
+   )
+
+   const handleGithubOAuthCode = async (code: string) => {
+      const response = await api.post('/register', { code })
+
+      const { token } = response.data
+
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('/memories')
+   }
+
+   useEffect(() => {
+      if (response?.type === 'success') {
+         const { code } = response.params
+
+         handleGithubOAuthCode(code)
+      }
+   }, [response])
 
    if (!hasLoadedFonts) return null
 
@@ -48,6 +88,7 @@ export default function App() {
             <TouchableOpacity
                activeOpacity={0.7}
                className="rounded-full bg-green-500 px-5 py-2"
+               onPress={() => promptAsync()}
             >
                <Text className="font-alt text-sm uppercase text-black">
                   COMEÇAR A CADASTRAR
